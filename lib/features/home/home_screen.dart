d031b6bd0f9270add666800f9cd40e72ff8f1ae3
ABR <rajeshregar482@gmail.com>
2024-10-15 11:19:28 +0530

    Posts Api Test Implemented.

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
           appBar: AppBar(
             centerTitle: true,
             title: Text(
-              "Posts",
+              "Posts".toUpperCase(),
               style: TextStyle(
                 fontSize: 18,
               ),
